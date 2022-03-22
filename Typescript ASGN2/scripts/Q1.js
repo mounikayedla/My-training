00d325f7c7cc746a818b6e1eec7161cd81952639
mounikayedla <89407537+mounikayedla@users.noreply.github.com>
2022-03-22 18:05:05 +0530

    Add files via upload

---
+++ Typescript ASGN2/scripts/Q1.js
@@ -0,0 +1,15 @@
+"use strict";
+var fib = function (previousNo, currentNo) {
+    var foo = previousNo + currentNo;
+    return {
+        current: foo,
+        next: function () {
+            return fib(currentNo, foo);
+        }
+    };
+};
+var generator = fib(2, 3);
+console.log(generator.current);
+var result = generator.next();
+console.log(result.current);
+//# sourceMappingURL=Q1.js.map
